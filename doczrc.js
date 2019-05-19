35d6342ffc4ef8f4426ccd89b84d320edba6702a
sandrina-p <sandrin4pereir4@gmail.com>
2019-05-19 01:51:29 +0100

    chore: Customize docz theme

--- doczrc.js
+++ doczrc.js
@@ -1,3 +1,17 @@
 export default {
   dest: '/docs',
+  themeConfig: {
+    styles: {
+      body: `
+        max-width: 60rem;
+        margin: auto;
+        font-size: 1rem;
+        line-height: 1.5;
+        font-family: "Source Sans Pro", sans-serif;
+      `,
+    },
+    colors: {
+      primary: 'teal',
+    },
+  },
 }
